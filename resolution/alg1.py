6decaf083be128627ebbd04a93a4404d31cf42e3
Jhones <jhonesaly@gmail.com>
2023-07-31 15:36:16 -0300

    feat: resolução do algoritmo 1 B

--- resolution/alg1.py
+++ resolution/alg1.py
@@ -4,13 +4,19 @@ input = 'teste'
 
 # for no input
 
-count = 0
+count_v = 0
+count_c = 0
+
 for str in input:
 
     # se a string for vogal, aumente o contador em 1
     if str in ['a','e','i','o','u']:
-        count += 1
+        count_v += 1
+    
+    elif str not in ['a','e','i','o','u']:
+        count_c += 1
 
 # print contagem
 
-print(count)
+print('o número de vogais é:', count_v)
+print('o número de consoantes é:', count_c)
