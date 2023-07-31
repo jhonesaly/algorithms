864d700c4040eaff22306c1494ac0018750daba0
Jhones <jhonesaly@gmail.com>
2023-07-31 15:54:09 -0300

    feat: resolução algoritmo 1 C

--- resolution/alg1.py
+++ resolution/alg1.py
@@ -1,20 +1,27 @@
 # Resolução algoritmo 1
 
-input = 'teste'
+input = '4aAAa 5CcCcC #$@'
 
-# for no input
+# Definições
 
 count_v = 0
 count_c = 0
+vogais = ['a','e','i','o','u']
+
+# Tratamento
+
+input = input.lower()
 
 for str in input:
 
-    # se a string for vogal, aumente o contador em 1
-    if str in ['a','e','i','o','u']:
-        count_v += 1
-    
-    elif str not in ['a','e','i','o','u']:
-        count_c += 1
+    # Verifica se a string é uma letra
+    if str.isalpha():
+        # se a string for vogal, aumente o contador de vogais em 1
+        if str in vogais:
+            count_v += 1
+        # se a string não for vogal, aumente contador de consoantes em 1
+        elif str not in vogais:
+            count_c += 1
 
 # print contagem
 
