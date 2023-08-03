cc8792dba108cdff1e1f8fbd248af1c0cdcd08e1
Jhones <jhonesaly@gmail.com>
2023-08-03 14:36:37 -0300

    feat: bubblesort with python

---
+++ bubblesort.py
@@ -0,0 +1,30 @@
+# Bubble sort com python
+
+def bubblesort(arr_list):
+
+    cont = True
+
+    while cont == True:
+
+        swapped = False
+
+        for i in range(len(arr_list)-1):
+
+            if arr_list[i] > arr_list[i+1]:
+                swapped = True
+                a_1 = arr_list[i]
+                a_2 = arr_list[i+1]
+                arr_list[i+1] = a_1
+                arr_list[i] = a_2
+
+        if swapped == False:
+            cont = False
+
+    return arr_list
+
+if __name__ == '__main__':
+
+    import unittest
+    from test_bubblesort import TestBubblesort
+
+    unittest.main()
