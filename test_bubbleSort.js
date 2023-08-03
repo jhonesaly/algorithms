9fbadf361e9fad7cb48ca3a4fd734e5eaced8af6
Jhones <jhonesaly@gmail.com>
2023-08-03 16:32:54 -0300

    fix: bug of bad typing

--- test_bubbleSort.js
+++ test_bubbleSort.js
@@ -9,7 +9,7 @@ describe('Bubble Sort', () => {
     });
 
     it('should sort a reverse range array in ascending order', () => {
-        const testInput = testInput = [5, 4, 3, 2, 1, 0];
+        const testInput = [5, 4, 3, 2, 1, 0];
         const expectedOutput = [0, 1, 2, 3, 4, 5];
         assert.deepStrictEqual(bubbleSort(testInput), expectedOutput);
     });
