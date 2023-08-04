b04aff2e2cab867e631d1dff5fc233ddd8987b21
Jhones <jhonesaly@gmail.com>
2023-08-04 17:50:41 -0300

    feat: remake of python code

--- bubblesort.py
+++ bubblesort.py
@@ -7,22 +7,19 @@ def bubblesort(arr_list):
         swapped = False
 
         for i in range(len(arr_list)-1):
-
             if arr_list[i] > arr_list[i+1]:
+                temp = arr_list[i]
+                arr_list[i] = arr_list[i+1]
+                arr_list[i+1] = temp
                 swapped = True
-                a_1 = arr_list[i]
-                a_2 = arr_list[i+1]
-                arr_list[i+1] = a_1
-                arr_list[i] = a_2
-
-        if swapped is False:
-            cont = False
-
+            
+        if not swapped:
+            break
+    
     return arr_list
 
 
 if __name__ == '__main__':
-
     import unittest
     from test_bubblesort import TestBubblesort
 
