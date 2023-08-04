b05bd1b585f867d201587e4634fee2705c767b9b
Jhones <jhonesaly@gmail.com>
2023-08-04 18:11:07 -0300

    feat: remake of javascript

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,23 +1,22 @@
-function bubbleSort(arr) {
+function bubbleSort(arr){
     let cont = true;
 
-    while (cont === true) {
+    while (cont) {
         let swapped = false;
 
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                swapped = true;
+        for (let i = 0; i < (arr.length - 1); i++) {
+            if (arr[i] > arr[i+1]) {
                 let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
             }
-        }
-
-        if (swapped === false) {
+        
+        if (swapped === false){
             cont = false;
         }
+        }
     }
-
     return arr;
 }
 
